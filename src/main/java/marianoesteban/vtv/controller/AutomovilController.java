093b306d0af810fc4c1e2dae63b621ac48102ea8
Mariano Esteban <marianoesteban@users.noreply.github.com>
2022-04-13 10:12:30 -0300

    Automóviles por estado en JSON

--- src/main/java/marianoesteban/vtv/controller/AutomovilController.java
+++ src/main/java/marianoesteban/vtv/controller/AutomovilController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,7 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.google.gson.Gson;
 
 import marianoesteban.vtv.model.Automovil;
 import marianoesteban.vtv.model.Marca;
@@ -126,5 +130,26 @@ public class AutomovilController {
 		model.addAttribute("automoviles", automovilService.getAutomovilesPorEstadoInspeccion("Rechazado"));
 		model.addAttribute("estadoInspeccion", "rechazados");
 		return "informes/automovil/estado";
+	}
+
+	@GetMapping(value = "/json/informes/automoviles/aptos", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String getAutomovilesAptosJson(Model model) {
+		Gson gson = new Gson();
+		return gson.toJson(automovilService.getAutomovilesPorEstadoInspeccion("Apto"));
+	}
+
+	@GetMapping(value = "/json/informes/automoviles/condicionales", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String getAutomovilesCondicionalesJson(Model model) {
+		Gson gson = new Gson();
+		return gson.toJson(automovilService.getAutomovilesPorEstadoInspeccion("Condicional"));
+	}
+
+	@GetMapping(value = "/json/informes/automoviles/rechazados", produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String getAutomovilesRechazadosJson(Model model) {
+		Gson gson = new Gson();
+		return gson.toJson(automovilService.getAutomovilesPorEstadoInspeccion("Rechazado"));
 	}
 }
